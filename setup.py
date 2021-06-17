9d75600d167626cd24d5cba929a980674d916786
myifengs <myifengs@gmail.com>
2021-06-17 13:03:45 +0800

    update version to 0.05

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read_file(filename):
 
 setup(
     name="article-parser",
-    version="0.0.4",
+    version="0.0.5",
     author="myifeng",
     author_email="myifengs@gmail.com",
     maintainer ="myifeng",
