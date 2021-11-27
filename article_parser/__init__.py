5437f1f497eacd190b65f8295f6204456b270220
myifengs <myifengs@gmail.com>
2021-11-27 14:08:25 +0800

    update options

--- article_parser/__init__.py
+++ article_parser/__init__.py
@@ -1,5 +1,5 @@
 from .Extractor import *
 
-def parse(url='', html=''):
-    ext = Extractor(url=url, html=html)
+def parse(url='', html='', options={}):
+    ext = Extractor(url=url, html=html, options=options)
     return ext.parse()
